7c6a53c0f7e84e62295a44cda16ba7d72cacc3da
nmshrkgch <nmshrkgch@gmail.com>
2019-10-06 15:12:48 +0900

    +を簡略化

--- 2018_yosen_01.py
+++ 2018_yosen_01.py
@@ -6,10 +6,10 @@ coin=a
 date=1
 day=1
 while coin<c:
-    coin=coin+a
-    date=date+1
-    day=day+1
+    coin+=a
+    date+=1
+    day+=1
     if date==7:
-        coin=coin+b
+        coin+=b
         date=0
 print(day)
