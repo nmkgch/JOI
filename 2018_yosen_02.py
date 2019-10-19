34b53d30124c02c14f49273abf31428d171c5b0b
nmshrkgch <nmshrkgch@gmail.com>
2019-10-19 16:20:02 +0900

    作成

---
+++ 2018_yosen_02.py
@@ -0,0 +1,26 @@
+cnt4=0
+s = [input() for i in range(4)]
+kazu=s[0]
+kazu=int(kazu)
+basyo=list(map(int, s[1].split()))
+sousa=s[2]
+sousa=int(sousa)
+bango=list(map(int, s[3].split()))
+for cnt in range(sousa):
+    cnt2=(bango[cnt])
+    cnt3=(basyo[cnt2-1])+1
+    if not cnt2==kazu:
+        cnt4=(basyo[cnt2])
+        del basyo[cnt2]
+    del basyo[cnt2-1]
+    if cnt3>=2019:
+        cnt3=2019
+    if cnt3==cnt4:
+        cnt3=cnt3-1
+    basyo.append(cnt3)
+    if not cnt2==kazu:
+        basyo.append(cnt4)
+    basyo.sort()
+moji=map(str,basyo)
+k='\n'.join(moji)
+print(k)
