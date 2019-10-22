6333969543a37f13f3937dfee88abff33ad2593c
nmshrkgch <nmshrkgch@gmail.com>
2019-10-22 17:26:49 +0900

    リストの繰り返しに変更

--- 2017_yosen_02.py
+++ 2017_yosen_02.py
@@ -1,22 +1,16 @@
-j=None
-m2=[]
-# s=[input() for i in range(2)]
-# N=int(s[0])
-# A=list(map(int, s[1].split()))
+s=[input() for i in range(2)]
+N=int(s[0])
+A=list(map(int, s[1].split()))
 
-N=7
-A=[0,0,1,0,1,1,0]
+renzoku = 0
+saidai = 0
 
-for t in range(N):
-    masu=A[0]
-    if masu==1 and j:
-        men+=1
-    elif masu==1:
-        men=1
-        j=True
-    elif j:
-        m2.append(men)        
-        j=False
-    del A[0]
-men=(max(m2))+1
-print(men)
+for i in A:
+    if i == 1:
+        renzoku += 1
+        if saidai < renzoku:
+            saidai = renzoku
+    else:
+        renzoku = 0
+
+print(saidai+1)
